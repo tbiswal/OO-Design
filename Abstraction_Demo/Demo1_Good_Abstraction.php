5695c7d9d333cf18c2158c0c7936aaabaa5f0b7f
tanmayatechs@gmail.com <tanmayatechs@gmail.com>
2020-07-24 13:01:11 +0530

    Change example for first demo to show good abstraction

--- Abstraction_Demo/Demo1_Good_Abstraction.php
+++ Abstraction_Demo/Demo1_Good_Abstraction.php
@@ -1,20 +1,14 @@
 <?php
 
-class Employee
+class AchievementBadge
 {
     // private data
-    private string $name;
-    private string $address;
-    private string $workPhone;
-    private string $homePhone;
-    private int $taxIdNumber;
-    private string $jobClassification;
+    private string $title;
+    private string $description;
+    private string $points;
+
 
     //public routines
-    public function getName(): string{}
-    public function getAddress(): string{}
-    public function getWorkPhone(): string{}
-    public function getHomePhone(): string{}
-    public function getTaxIdNumber(): int{}
-    public function getJobClassification(): string{}
+    public function awardTo($userId): bool {}
+    public function pastAchievements($userId): array {}
 }
